b6b24c8ea487df802fe4aad748ff6a6e445f7a5e
Randal Rand <randal.rand@bpiedu.com>
2013-04-30 14:53:30 -0700

    added the ability to remove handler mapping.

--- Services.Course.Host/PreDeploy.ps1
+++ Services.Course.Host/PreDeploy.ps1
@@ -31,6 +31,31 @@ Get-ChildItem $PSScriptRoot -Include "*.Release.config", "*.$OctopusEnvironmentN
 # check configure iis
 Deployment-CheckConfigureIIS
 
+
+
+##############################
+# Deploy Website
+
+
+# setup website
+Deployment-SetupWebsite -WebsiteName "$IISWebsiteName" -WebsiteDirectoryPath "$IISWebsitePath" -AppPoolName "$IISWebsiteAppPoolName"
+Deployment-SetWebsiteBinding -WebsiteName "$IISWebsiteName" -Protocol "http" -IPAddress "*" -Port "80" -HostHeader "$IISHostHeaders"
+
+# setup SSL
+Deployment-SetWebsiteBinding -WebsiteName "$IISWebsiteName" -Protocol "https" -IPAddress "*" -Port "443" -HostHeader "$IISHostHeaders"
+Deployment-AssociateSSLCertificateWithBinding -CertThumbprint "$IISSslCertThumbprint" -CertStoreScope "LocalMachine" -CertStoreName "My" -BindingPath "0.0.0.0!443"
+
+# setup app pool
+Deployment-SetAppPoolProperty -AppPoolName "$IISWebsiteAppPoolName" -AppPoolProperty 'ManagedRuntimeVersion'     -AppPoolPropertyValue 'v4.0'
+Deployment-SetAppPoolProperty -AppPoolName "$IISWebsiteAppPoolName" -AppPoolProperty 'ManagedPipelineMode'       -AppPoolPropertyValue ([int] [Microsoft.Web.Administration.ManagedPipelineMode]::Identity)          -CompareValue 'Integrated'   #Integrated = 0, Classic = 1
+Deployment-SetAppPoolProperty -AppPoolName "$IISWebsiteAppPoolName" -AppPoolProperty 'ProcessModel.IdentityType' -AppPoolPropertyValue ([int] [Microsoft.Web.Administration.ProcessModelIdentityType]::SpecificUser) -CompareValue 'SpecificUser' #LocalSystem = 0, LocalService = 1, NetworkService = 2, SpecificUser = 3, ApplicationPoolIdentity = 4
+Deployment-SetAppPoolProperty -AppPoolName "$IISWebsiteAppPoolName" -AppPoolProperty 'ProcessModel.Username'     -AppPoolPropertyValue $IISWebsiteAppPoolIdentity
+Deployment-SetAppPoolProperty -AppPoolName "$IISWebsiteAppPoolName" -AppPoolProperty 'ProcessModel.Password'     -AppPoolPropertyValue $IISWebsiteAppPoolIdentityPassword -Hide
+
+# Removing default IIS setting OPTIONVebHandler
+Deployment-RemoveHandlerMapping -HandlerName "OPTIONSVerbHandler" -WebsiteName "$IISWebsiteName"
+
+
 # setup web application
 Deployment-SetupWebApplication -WebsiteName "$IISWebsiteName" -WebApplicationName "$IISWebApplicationName" -WebApplicationDirectoryPath "$OctopusPackageDirectoryPath" -AppPoolName "$IISAppPoolName"
 
