2487e8636b63291a1b133aedd233c0f568f6f394
Sang Yum <sangyum@gmail.com>
2013-05-15 10:39:55 -0700

    remove the need for ProgramCourse link table

--- Services.Course.Domain/Overrides/CourseMappingOverride.cs
+++ Services.Course.Domain/Overrides/CourseMappingOverride.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BpeProducts.Services.Course.Domain.Entities;
 using FluentNHibernate.Automapping;
 using FluentNHibernate.Automapping.Alterations;
 
---
+++ Services.Course.Domain/Overrides/ProgramMappingOverride.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace BpeProducts.Services.Course.Domain.Overrides
+{
+    public class ProgramMappingOverride : IAutoMappingOverride<Entities.Program>
+    {
+        public void Override(AutoMapping<Entities.Program> mapping)
+        {
+            mapping.HasMany(p => p.Courses).Inverse();
+        }
+    }
+}
+
