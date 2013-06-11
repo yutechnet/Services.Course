a3e8c983beaa8e2abf87a251a64798147e5b7458
Bryce Ashey <bryce.ashey@bpiedu.com>
2013-06-11 15:24:52 -0700

    Added [DataMember(IsRequired=true)] to contract.

--- Services.Course.Contract/SaveProgramRequest.cs
+++ Services.Course.Contract/SaveProgramRequest.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace BpeProducts.Services.Course.Contract
 {
+    [DataContract]
 	public class SaveProgramRequest
 	{
 		[Required]
 		public string Name { get; set; }
 		[Required]
 		public string Description { get; set; }
-        [Required]
+        [Required, DataMember(IsRequired= true)]
         public Guid OrganizationId { get; set; }
 		[Required]
 		public string TenantId { get; set; }
