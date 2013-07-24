8ac3eee373a077e4c13ba4d0fe78ff23dd45e15c
Amro Ali <amro76@gmail.com>
2013-07-24 13:39:49 -0700

    adding files this time

---
+++ Services.Course.Host.Tests.Integration/CoursePreRequisite.feature
@@ -0,0 +1,34 @@
+﻿@Api
+Feature: CoursePrerequisites
+	As a course creator
+	I want to be able to declare the set of courses that need to be accomplished prior to a given course
+	So that students can be enrolled in these courses
+
+Background: 
+	Given the following courses are published:
+	| Name     | Code | Description           | OrganizationId                       | CourseType  | IsTemplate |
+	| Math 101 | M101 | Basic mathematics     | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+	| Econ 400 | E400 | Advanced Econometrics | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+	| Poly 220 | P220 | Comparative Politics  | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+
+Scenario: Add a course prerequisite
+	When I add the following prerequisites to 'Econ 400'
+	| Name     | Code | Description           | OrganizationId                       | CourseType  | IsTemplate |
+	| Econ 200 | E200 | Intro to Econometrics | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+	| Econ 100 | E100 | Macroeconomics        | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+	Then the course 'Econ 400' should have the following prerequisites
+	| Name     | Code | Description           | OrganizationId                       | CourseType  | IsTemplate |
+	| Econ 200 | E200 | Intro to Econometrics | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+	| Econ 100 | E100 | Macroeconomics        | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+
+Scenario: Cannot add a prerequisite to a course that is not published
+	Given the following courses exist:
+		| Name     | Code | Description | OrganizationId                       | CourseType  | IsTemplate |
+		| Math 200 | M200 | Calculus    | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+	When I add the following prerequisites to 'Math 200'
+		| Name     | Code | Description       | OrganizationId                       | CourseType  | IsTemplate |
+		| Math 101 | M101 | Basic mathematics | E2DF063D-E2A1-4F83-9BE0-218EC676C05F | Traditional | False      |
+	Then I get 'BadRequest' response
+
+Scenario: Cannot add a prerequisite to a course using the wrong organization id
+Scenario: Verify pre-req course id is the version id
---
+++ Services.Course.Host.Tests.Integration/CoursePrerequisitesSteps.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using BpeProducts.Services.Course.Contract;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using System.Linq;
+
+namespace BpeProducts.Services.Course.Host.Tests.Integration
+{
+    [Binding]
+    public class CoursePrerequisitesSteps
+    {
+        private const string Url = "/course";
+
+        [Given(@"the following courses are published:")]
+        public void GivenTheFollowingCoursesArePublished(Table table)
+        {
+            foreach (var row in table.Rows)
+            {
+                var courseRequest = new SaveCourseRequest
+                {
+                    Name = row["Name"],
+                    Code = row["Code"],
+                    Description = row["Description"],
+                    TenantId = 999999,
+                    OrganizationId = new Guid(table.Rows[0]["OrganizationId"]),
+                    PrerequisiteCourseIds = new List<Guid>()
+                };
+
+                var request = ApiFeature.ApiTestHost.Client.PostAsJsonAsync(Url, courseRequest).Result;
+                request.EnsureSuccessStatusCode();
+                var location = request.Headers.Location;
+
+                var publishRequest = new PublishRequest() { PublishNote = "this is now published" };
+                var publishUri = string.Format("{0}/publish", location);
+                var publishResponse = ApiFeature.ApiTestHost.Client.PutAsJsonAsync(publishUri, publishRequest).Result;
+                publishResponse.EnsureSuccessStatusCode();
+
+                ScenarioContext.Current.Add(courseRequest.Name, request.Headers.Location);
+            }
+        }
+
+
+        [When(@"I add the following prerequisites to '(.*)'")]
+        public void WhenIAddTheFollowingPrerequisitesTo(string courseName, Table table)
+        {
+            foreach (var row in table.Rows)
+            {
+                var preReqRequest = new SaveCourseRequest
+                {
+                    Name = row["Name"],
+                    Code = row["Code"],
+                    Description = row["Description"],
+                    TenantId = 999999,
+                    OrganizationId = new Guid(table.Rows[0]["OrganizationId"]),
+                    PrerequisiteCourseIds = new List<Guid>()
+                };
+
+                var putUrl = string.Format("{0}/prerequisites", ScenarioContext.Current.Get<Uri>(courseName));
+                var response = ApiFeature.ApiTestHost.Client.PutAsync(putUrl, preReqRequest, new JsonMediaTypeFormatter()).Result;
+                var courseInfo = response.Content.ReadAsAsync<CourseInfoResponse>().Result;
+                ScenarioContext.Current.Add(preReqRequest.Name, courseInfo);
+            }
+        }
+
+        [Then(@"the course '(.*)' should have the following prerequisites")]
+        public void ThenTheCourseShouldHaveTheFollowingPrerequisites(string courseName, Table table)
+        {
+            var getUri = ScenarioContext.Current.Get<Uri>(courseName);
+            var response = ApiFeature.ApiTestHost.Client.GetAsync(getUri.ToString()).Result;
+            response.EnsureSuccessStatusCode();
+            var courseInfo = ScenarioContext.Current.Get<CourseInfoResponse>(courseName);
+
+            var listOfPreReqs = courseInfo.PrerequisiteCourseIds;
+            var getResponse = response.Content.ReadAsAsync<CourseInfoResponse>().Result;
+            Assert.AreEqual(getResponse.PrerequisiteCourseIds, listOfPreReqs);
+        }
+    }
+}
