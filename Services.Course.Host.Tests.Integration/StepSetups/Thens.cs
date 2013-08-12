702a3be47d1ef177e487e5a583448de84596ca08
John Krlin <jkrlin.spm@gmail.com>
2013-08-12 15:14:04 -0700

    add missing Then step

--- Services.Course.Host.Tests.Integration/StepSetups/Thens.cs
+++ Services.Course.Host.Tests.Integration/StepSetups/Thens.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using BpeProducts.Services.Course.Contract;
 using BpeProducts.Services.Course.Host.Tests.Integration.Operations;
+using BpeProducts.Services.Course.Host.Tests.Integration.Resources;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
@@ -26,7 +27,7 @@ namespace BpeProducts.Services.Course.Host.Tests.Integration.StepSetups
         {
             var courseResource = Givens.Courses[courseName];
 
-            var expectedCode = (HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), status);
+            var expectedCode = (HttpStatusCode) Enum.Parse(typeof (HttpStatusCode), status);
             Assert.That(courseResource.Response.StatusCode, Is.EqualTo(expectedCode));
         }
 
@@ -41,7 +42,7 @@ namespace BpeProducts.Services.Course.Host.Tests.Integration.StepSetups
             CollectionAssert.AreEquivalent(course.ProgramIds, expectedProgramIds);
         }
 
-        [Then(@"'(.*)' program is associated with the following learning outcomes:")]
+        [Then(@"'(.*)' program is associated with the following learning outcomes")]
         public void ThenProgramIsAssociatedWithTheFollowingLearningOutcomes(string programName, Table table)
         {
             var resource = Givens.Programs[programName];
@@ -93,9 +94,21 @@ namespace BpeProducts.Services.Course.Host.Tests.Integration.StepSetups
         {
             var response = Whens.ResponseMessages.Last();
 
-            var expectedStatusCode = (HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), status);
+            var expectedStatusCode = (HttpStatusCode) Enum.Parse(typeof (HttpStatusCode), status);
 
             Assert.That(response.StatusCode.Equals(expectedStatusCode));
+        }
+
+        [Then(@"I get the following responses")]
+        public void ThenIGetTheFollowingResponses(Table table)
+        {
+            for (var i = 0; i < table.Rows.Count; i++)
+            {
+                var result = Whens.ResponseMessages[Whens.ResponseMessages.Count - table.Rows.Count + i];
+                var statusCode = (HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), table.Rows[i]["StatusCode"]); 
+
+                Assert.That(result.StatusCode, Is.EqualTo(statusCode));
+            }
         }
 
         [Then(@"my course learning activity '(.*)' contains the following")]
@@ -108,7 +121,7 @@ namespace BpeProducts.Services.Course.Host.Tests.Integration.StepSetups
             table.CompareToInstance(courseLearningActivity);
         }
 
- 	[Then(@"the program '(.*)' include the following course information:")]
+        [Then(@"the program '(.*)' include the following course information:")]
         public void ThenTheProgramIncludeTheFollowingCourseInformation(string programName, Table table)
         {
             var programResource = Givens.Programs[programName];
@@ -119,13 +132,14 @@ namespace BpeProducts.Services.Course.Host.Tests.Integration.StepSetups
             CollectionAssert.AreEquivalent(program.Courses.Select(c => c.Id).ToList(), courseIds);
         }
 
-	[Then(@"the segment '(.*)' should have the following learning activities:")]
+        [Then(@"the segment '(.*)' should have the following learning activities:")]
         public void ThenTheSegmentShouldHaveTheFollowingLearningActivities(string segmentName, Table table)
         {
             var resource = Givens.Segments[segmentName];
             var segment = GetOperations.GetSegment(resource.ResourseUri);
 
-            var expectedLearningActivities = (from r in table.Rows select Givens.CourseLearningActivities[r["Name"]].Id).ToList();
+            var expectedLearningActivities =
+                (from r in table.Rows select Givens.CourseLearningActivities[r["Name"]].Id).ToList();
             var actualLearningActivities = (from a in segment.CourseLearningActivities select a.Id).ToList();
 
             CollectionAssert.AreEquivalent(expectedLearningActivities, actualLearningActivities);
@@ -156,7 +170,8 @@ namespace BpeProducts.Services.Course.Host.Tests.Integration.StepSetups
         {
             var resource = Givens.Programs[programName];
 
-            var actualOutcomes = (from o in GetOperations.GetSupportedOutcomes(resource.ResourceUri) select o.Description).ToList();
+            var actualOutcomes =
+                (from o in GetOperations.GetSupportedOutcomes(resource.ResourceUri) select o.Description).ToList();
             var expectedOutcomes = (from o in table.Rows select o["Description"]).ToList();
 
             CollectionAssert.AreEquivalent(expectedOutcomes, actualOutcomes);
@@ -167,10 +182,63 @@ namespace BpeProducts.Services.Course.Host.Tests.Integration.StepSetups
         {
             var resource = Givens.LearningOutcomes[learningOutcomeName];
 
-            var actualOutcomes = (from o in GetOperations.GetSupportedOutcomes(resource.ResourceUri) select o.Description).ToList();
+            var actualOutcomes =
+                (from o in GetOperations.GetSupportedOutcomes(resource.ResourceUri) select o.Description).ToList();
             var expectedOutcomes = (from o in table.Rows select o["Description"]).ToList();
 
             CollectionAssert.AreEquivalent(expectedOutcomes, actualOutcomes);
         }
+
+        [Then(@"I get the entity learning outcomes as follows:")]
+        public void ThenIGetTheEntityLearningOutcomesAsFollows(Table table)
+        {
+            var expectedEntityOutcomes = new Dictionary<IResource, IList<Guid>>();
+            foreach (var row in table.Rows)
+            {
+                var entityType = row["EntityType"];
+                var entityName = row["EntityName"];
+                IResource resource = null;
+
+                switch (entityType)
+                {
+                    case "Program":
+                        resource = Givens.Programs[entityName];
+                        break;
+                    case "Course":
+                        resource = Givens.Courses[entityName];
+                        break;
+                    case "Segment":
+                        resource = Givens.Segments[entityName];
+                        break;
+                }
+
+                if (resource == null)
+                    throw new Exception("No recourse found for entity type " + entityType);
+
+                var expectedOutcomes = (from o in row["LearningOutcomes"].Split(new[] {','})
+                                        where !string.IsNullOrWhiteSpace(o)
+                                        select Givens.LearningOutcomes[o.Trim()].Id).ToList();
+                expectedEntityOutcomes.Add(resource, expectedOutcomes);
+            }
+
+            var entityIdsToGet = (from e in expectedEntityOutcomes.Keys select e.Id).ToList();
+            var actualEntityOutcomes = GetOperations.GetEntityLearningOutcomes(entityIdsToGet);
+
+            foreach (var expectedEntity in expectedEntityOutcomes)
+            {
+                List<OutcomeInfo> outcomes;
+
+                if (actualEntityOutcomes.TryGetValue(expectedEntity.Key.Id, out outcomes))
+                {
+                    var actualEntityOutcomeIds = (from o in outcomes select o.Id).ToList();
+                    CollectionAssert.AreEquivalent(expectedEntity.Value.ToList(), actualEntityOutcomeIds);
+                }
+                else
+                {
+                    Assert.That(!actualEntityOutcomes.Keys.Contains(expectedEntity.Key.Id));
+                }
+            }
+        }
     }
 }
+
