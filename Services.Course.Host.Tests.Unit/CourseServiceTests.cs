024c673209bd7b7cdbaee83f2a3dbaaea16aa2e4
RaymondYu <raymond.yu@bpiedu.com>
2013-08-23 16:11:48 -0700

    add event play for course prerequisites add and remove

--- Services.Course.Host.Tests.Unit/CourseServiceTests.cs
+++ Services.Course.Host.Tests.Unit/CourseServiceTests.cs
@@ -35,8 +35,13 @@ namespace BpeProducts.Services.Course.Host.Tests.Unit
             var courseToReturn = new Domain.Courses.Course {Id = Guid.NewGuid(), ActiveFlag = true};
 			_repoMock.Setup(r => r.Get(It.IsAny<Guid>())).Returns(courseToReturn);
 
-            var newPrerequisiteList = new List<Guid> {Guid.NewGuid()};
-            _courseService.UpdatePrerequisiteList(Guid.NewGuid(), newPrerequisiteList);
+            var courseToBePrerequisite = new Domain.Courses.Course { Id = Guid.NewGuid(), ActiveFlag = true };
+            var prereqCoursesInDb = new List<Course.Domain.Courses.Course> {courseToBePrerequisite };
+
+            _repoMock.Setup(c => c.Get(It.IsAny<List<Guid>>())).Returns(prereqCoursesInDb);
+
+            var newPrerequisiteList = new List<Guid> {courseToBePrerequisite.Id};
+            _courseService.UpdatePrerequisiteList(courseToReturn.Id, newPrerequisiteList);
 
             _domainEventsMock.Verify(d => d.Raise<CoursePrerequisiteAdded>(It.IsAny<CoursePrerequisiteAdded>()), Times.Exactly(1));
             _domainEventsMock.Verify(d => d.Raise<CoursePrerequisiteRemoved>(It.IsAny<CoursePrerequisiteRemoved>()), Times.Exactly(0));
@@ -48,12 +53,15 @@ namespace BpeProducts.Services.Course.Host.Tests.Unit
 			var prerequisiteCourse = new Domain.Courses.Course { Id = Guid.NewGuid() };
 			prerequisiteCourse.Publish("");
 
+           var prereqCoursesInDb = new List<Course.Domain.Courses.Course> { prerequisiteCourse };
+            _repoMock.Setup(c => c.Get(It.IsAny<List<Guid>>())).Returns(prereqCoursesInDb);
+
             var courseToReturn = new Domain.Courses.Course { Id = Guid.NewGuid(), ActiveFlag = true };
 			courseToReturn.AddPrerequisite(prerequisiteCourse);
 			_repoMock.Setup(c => c.Get(It.IsAny<Guid>())).Returns(courseToReturn);
 
             var newPrerequisiteList = new List<Guid>();
-            _courseService.UpdatePrerequisiteList(Guid.NewGuid(), newPrerequisiteList);
+            _courseService.UpdatePrerequisiteList(courseToReturn.Id, newPrerequisiteList);
 
             _domainEventsMock.Verify(d => d.Raise<CoursePrerequisiteAdded>(It.IsAny<CoursePrerequisiteAdded>()), Times.Exactly(0));
             _domainEventsMock.Verify(d => d.Raise<CoursePrerequisiteRemoved>(It.IsAny<CoursePrerequisiteRemoved>()), Times.Exactly(1));
@@ -64,18 +72,24 @@ namespace BpeProducts.Services.Course.Host.Tests.Unit
         {
             var guid1 = Guid.NewGuid();
             var guid2 = Guid.NewGuid();
-	        var prerequisiteCourse = new Domain.Courses.Course {Id = guid1};
+            var guid3 = Guid.NewGuid();
+
+            var prerequisiteCourse = new Domain.Courses.Course {Id = guid1};
 	        var prerequisiteCourse2 = new Domain.Courses.Course {Id = guid2};
+            
+            var prerequisiteCourseToBeAdded = new Domain.Courses.Course {Id = guid3};
 			prerequisiteCourse.Publish("");
 			prerequisiteCourse2.Publish("");
+            var prereqCoursesInDb = new List<Course.Domain.Courses.Course> {prerequisiteCourse, prerequisiteCourse2,prerequisiteCourseToBeAdded};
 
             var courseToReturn = new Domain.Courses.Course { Id = Guid.NewGuid(), ActiveFlag = true };
 			courseToReturn.AddPrerequisite(prerequisiteCourse);
 			courseToReturn.AddPrerequisite(prerequisiteCourse2);
 			_repoMock.Setup(c => c.Get(It.IsAny<Guid>())).Returns(courseToReturn);
+            _repoMock.Setup(c => c.Get(It.IsAny<List<Guid>>())).Returns(prereqCoursesInDb);
 
-            var newPrerequisiteList = new List<Guid> { guid2, Guid.NewGuid() };
-            _courseService.UpdatePrerequisiteList(Guid.NewGuid(), newPrerequisiteList);
+            var newPrerequisiteList = new List<Guid> { guid2, prerequisiteCourseToBeAdded.Id };
+            _courseService.UpdatePrerequisiteList(courseToReturn.Id, newPrerequisiteList);
 
             _domainEventsMock.Verify(d => d.Raise<CoursePrerequisiteAdded>(It.IsAny<CoursePrerequisiteAdded>()), Times.Exactly(1));
             _domainEventsMock.Verify(d => d.Raise<CoursePrerequisiteRemoved>(It.IsAny<CoursePrerequisiteRemoved>()), Times.Exactly(1));
