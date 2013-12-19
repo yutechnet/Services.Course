e3fc275081297b72d80af026d2ed16520189d348
kevingujiang <kgu@contractor.bpiedu.com>
2013-12-19 17:14:29 +0800

    add the missing file

---
+++ Services.Course.Contract/UpdateLearningMaterialRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpeProducts.Services.Course.Contract
+{
+    public class UpdateLearningMaterialRequest
+    {
+        public string Instruction { get; set; }
+        public bool IsRequired { get; set; }
+        public Guid CourseSegmentId { get; set; }
+        public Guid AssetId { get; set; }
+    }
+}
