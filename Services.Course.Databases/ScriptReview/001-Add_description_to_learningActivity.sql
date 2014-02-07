24c2a5c4aeab52002bdc6c3827095f3aa0c091bc
Matthew Stoddart <stoddart.m@gmail.com>
2014-02-06 16:11:36 -0800

    Added script for learningActivity description

---
+++ Services.Course.Databases/ScriptReview/001-Add_description_to_learningActivity.sql
@@ -0,0 +1,13 @@
+﻿Use Course
+go
+If exists(select 1 from sys.columns c where c.name = 'Description' and c.object_id = Object_id('CourseLearningActivity'))
+begin
+print 'column exists already, skip creation';
+end
+else
+begin
+
+ALTER TABLE CourseLearningActivity add [Description] nvarchar(255) NULL;
+print 'column added successfully';
+end
+go
