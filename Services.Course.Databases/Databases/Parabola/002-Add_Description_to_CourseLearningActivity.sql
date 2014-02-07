7c12297d1d5276e394c61c4451f4f46680ed832f
akarcher-bpe <andrew.karcher@bpiedu.com>
2014-02-07 11:28:31 -0800

    Reviewed and Moved Script to DB Folder

---
+++ Services.Course.Databases/Databases/Parabola/002-Add_Description_to_CourseLearningActivity.sql
@@ -0,0 +1,21 @@
+﻿
+IF EXISTS (Select 1 From INFORMATION_SCHEMA.COLUMNS c 
+			Where c.COLUMN_NAME = 'Description' and c.TABLE_NAME = 'CourseLearningActivity')
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'NO CHANGE: Column (Description) Already Exists on Table (CourseLearningActivity)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+ELSE
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE BEGIN: Column (Description) being added to Table (CourseLearningActivity)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	
+	ALTER TABLE [CourseLearningActivity] add [Description] nvarchar(1000) NULL;
+
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE END: Column (Description) being added to Table (CourseLearningActivity)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+
