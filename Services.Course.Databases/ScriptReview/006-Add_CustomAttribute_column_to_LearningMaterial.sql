619667375be6d776a1bb490895c7b22528b3bcbf
Anthony <age@contractor.bpiedu.com>
2014-03-17 10:59:39 +0800

    US3283 TA5191 add sql script for adding customattribute column into learningmatrial

---
+++ Services.Course.Databases/ScriptReview/006-Add_CustomAttribute_column_to_LearningMaterial.sql
@@ -0,0 +1,21 @@
+﻿IF EXISTS (Select 1 From INFORMATION_SCHEMA.COLUMNS c 
+			Where c.COLUMN_NAME = 'CustomAttribute' and c.TABLE_NAME = 'LearningMaterial')
+								  
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'NO CHANGE: Column (CustomAttribute) Already Exists on Table (LearningMaterial)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+ELSE
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE BEGIN: Column (CustomAttribute) being added to Table (LearningMaterial)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	
+	ALTER TABLE [LearningMaterial] add [CustomAttribute] nvarchar(250) NULL;
+
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE END: Column (CustomAttribute) being added to Table (LearningMaterial)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+
