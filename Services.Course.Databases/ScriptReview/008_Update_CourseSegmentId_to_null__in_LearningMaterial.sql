d699c648add21ffeab50d1e9f3e3f8d462b8f041
Anthony <age@contractor.bpiedu.com>
2014-03-20 15:48:30 +0800

    US3284 TA5257 add sql script for set courseSegmentId to allow null

---
+++ Services.Course.Databases/ScriptReview/008_Update_CourseSegmentId_to_null__in_LearningMaterial.sql
@@ -0,0 +1,20 @@
+﻿IF EXISTS (Select * From INFORMATION_SCHEMA.COLUMNS c 
+			Where c.COLUMN_NAME = 'CourseSegmentId' and c.TABLE_NAME = 'LearningMaterial' AND IS_NULLABLE='NO')
+	BEGIN
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( 'CREATE BEGIN: Column (CourseSegmentId) of Table (LearningMaterial) being updated to null' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+
+		ALTER TABLE LearningMaterial ALTER COLUMN CourseSegmentId UNIQUEIDENTIFIER NULL;
+
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( 'CREATE END: Column (CourseSegmentId) of Table (LearningMaterial) being updated to null' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	END
+ELSE
+	BEGIN
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( 'NO CHANGE: Column (CourseSegmentId) of Table (LearningMaterial) being updated to null' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	END
+GO
