db1e542c1ce409b3bcd2cc85808d713d79c48296
RaymondYu <raymond.yu@bpiedu.com>
2014-06-03 18:24:39 -0700

    US3913 fix wrong data type for IsActivated on Course table

---
+++ Services.Course.Databases/Databases/Parabola/015-Update_IsActivated_to_Bit_Course.sql
@@ -0,0 +1,20 @@
+﻿IF EXISTS (Select 1 From INFORMATION_SCHEMA.COLUMNS c 
+			Where c.COLUMN_NAME = 'IsActivated' and c.TABLE_NAME = 'Course' AND IS_NULLABLE='Yes')
+	BEGIN
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( 'CREATE BEGIN: Column (IsActivated) of Table (Course) being updated to null' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+
+		ALTER TABLE Course ALTER COLUMN IsActivated bit NULL;
+
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( 'CREATE END: Column (IsActivated) of Table (Course) being updated to null' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	END
+ELSE
+	BEGIN
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( 'NO CHANGE: Column (IsActivated) of Table (Course) being updated to null' , 0 , 1 )WITH NOWAIT;
+		RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	END
+GO
