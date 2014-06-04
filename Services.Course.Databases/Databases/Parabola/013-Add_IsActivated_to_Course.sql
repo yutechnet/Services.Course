54adccd5e910ab0e18bcb1c8382e15b70fb14904
RaymondYu <raymond.yu@bpiedu.com>
2014-06-03 18:10:15 -0700

    US3913 Add IsActivated column to Course table

---
+++ Services.Course.Databases/Databases/Parabola/013-Add_IsActivated_to_Course.sql
@@ -0,0 +1,21 @@
+﻿
+IF EXISTS (Select 1 From INFORMATION_SCHEMA.COLUMNS c 
+			Where c.COLUMN_NAME = 'IsActivated' and c.TABLE_NAME = 'Course')
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'NO CHANGE: Column (IsActivated) Already Exists on Table (Course)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+ELSE
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE BEGIN: Column (IsActivated) being added to Table (Course)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	
+	ALTER TABLE [Course] add IsActivated nvarchar(max) NULL;
+
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE END: Column (IsActivated) being added to Table (Course)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+
