f2631d37f0ddeea2cf8bdd167dbb7b31e15b28cf
Anthony <age@contractor.bpiedu.com>
2014-07-15 16:19:32 +0800

    US4443 TA8340 change customAttribute name to MetaData in Learning Activity and learning Material table

---
+++ Services.Course.Databases/ScriptReview/020-Change_CustomAttribute_name_to_MetaData_in_LearningActivity.sql
@@ -0,0 +1,21 @@
+﻿IF NOT EXISTS (Select 1 From INFORMATION_SCHEMA.COLUMNS c 
+			Where c.COLUMN_NAME = 'CustomAttribute' and c.TABLE_NAME = 'LearningActivity')
+								  
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'NO CHANGE: Column (CustomAttribute) Does Not Exists on Table (LearningActivity)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+ELSE
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE BEGIN: Column (CustomAttribute) being changed name(MetaData) to Table (LearningActivity)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	
+	EXEC sp_rename N'dbo.LearningActivity.CustomAttribute', N'MetaData', N'Column';
+
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE END: Column (CustomAttribute) being changed name(MetaData) to Table (LearningActivity)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+
