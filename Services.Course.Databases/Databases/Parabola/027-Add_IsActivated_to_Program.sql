e8f85f70577577375b43320794821da815c238ce
Kevin Gu <kgu@contractor.bpiedu.com>
2014-08-01 16:56:18 +0800

    add IsActivated to program

---
+++ Services.Course.Databases/Databases/Parabola/027-Add_IsActivated_to_Program.sql
@@ -0,0 +1,21 @@
+﻿
+IF EXISTS (Select 1 From INFORMATION_SCHEMA.COLUMNS c 
+			Where c.COLUMN_NAME = 'IsActivated' and c.TABLE_NAME = 'Program')
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'NO CHANGE: Column (IsActivated) Already Exists on Table (Program)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+ELSE
+BEGIN
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE BEGIN: Column (IsActivated) being added to Table (Program)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	
+	ALTER TABLE [Program] add IsActivated bit NULL;
+
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( 'CREATE END: Column (IsActivated) being added to Table (Program)' , 0 , 1 )WITH NOWAIT;
+	RAISERROR( '-----------------------------' , 0 , 1 )WITH NOWAIT;
+END
+
