4eeb346334fdca423a02eedff1c6df4799056a38
russellchadwick <russell.chadwick@bpiedu.com>
2014-08-27 14:32:00 -0700

    Add ApiVersion to SectionClient

--- Services.Course.Domain/SectionClient.cs
+++ Services.Course.Domain/SectionClient.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using BpeProducts.Common.Authorization;
+using BpeProducts.Common.Contract;
 using BpeProducts.Services.Section.Contracts;
 
 namespace BpeProducts.Services.Course.Domain
@@ -37,7 +38,7 @@ namespace BpeProducts.Services.Course.Domain
         {
             var samlToken = _tokenExtractor.GetSamlToken();
 			var xApiKey = _tokenExtractor.GetApiKey();
-            var client = new HttpClient();
+            var client = HttpClientFactory.Create(new ApiVersionMessageHandler(ApiVersions.Version1Aug292014Release));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("SAML", samlToken);
 			client.DefaultRequestHeaders.Add("X-ApiKey", xApiKey);
             var uri = new Uri(sectionServiceAddress.ToString().TrimEnd('/') + "/section");
