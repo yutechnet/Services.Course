e2dc51bba438aa60ce893eeb8043b78891f92786
russellchadwick <russell.chadwick@bpiedu.com>
2014-09-15 09:30:58 -0700

    Update section client to newer method of service to service calls

--- Services.Course.Domain/SectionClient.cs
+++ Services.Course.Domain/SectionClient.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Configuration;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using BpeProducts.Common.Authorization;
 using BpeProducts.Common.Contract;
 using BpeProducts.Services.Section.Contracts;
@@ -15,26 +14,25 @@ namespace BpeProducts.Services.Course.Domain
 
     public class SectionClient : ISectionClient
     {
-        private readonly ISamlTokenExtractor _tokenExtractor;
-        public Uri BaseAddress { get; private set; }
+        public Lazy<HttpClient> HttpClient { get; set; }
 
-        public SectionClient(ISamlTokenExtractor tokenExtractor)
+        public SectionClient(RequestContext requestContext)
         {
-            _tokenExtractor = tokenExtractor;
-            BaseAddress = new Uri(ConfigurationManager.AppSettings["SectionServiceBaseUrl"]);
-
+            HttpClient = new Lazy<HttpClient>(() =>
+                {
+                    var httpClient = HttpClientFactory.Create(
+                        new RequestIdMessageHandler(requestContext.RequestId),
+                        new ApiVersionMessageHandler(ApiVersions.Version1Aug292014Release),
+                        new SignatureMessageHandler(requestContext.ApiKey.Value, requestContext.UserId.Value,
+                                                    requestContext.SharedSecret));
+                    httpClient.BaseAddress = new Uri(ConfigurationManager.AppSettings["SectionServiceBaseUrl"]);
+                    return httpClient;
+                });
         }
 
         public HttpResponseMessage CreateSection(CreateSectionRequest request)
         {
-            var samlToken = _tokenExtractor.GetSamlToken();
-			var xApiKey = _tokenExtractor.GetApiKey();
-            var client = HttpClientFactory.Create(new ApiVersionMessageHandler(ApiVersions.Version1Aug292014Release));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("SAML", samlToken);
-			client.DefaultRequestHeaders.Add("X-ApiKey", xApiKey);
-            var uri = new Uri(BaseAddress + "section");
-            var response = client.PostAsJsonAsync(uri.ToString(), request);
-
+            var response = HttpClient.Value.PostAsJsonAsync("section", request);
             return response.Result;
         }
     }
